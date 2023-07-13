436c45f3337ea0e127349a3d3f6eee64eaf07ff8
Kiarash Vosough <vosough.k@gmail.com>
2023-07-14 00:56:20 +0330

    updated AlbumListReducer models

--- MyAlbum/Presentation/AlbumList/AlbumListReducer.swift
+++ MyAlbum/Presentation/AlbumList/AlbumListReducer.swift
@@ -11,23 +11,18 @@ import Dependencies
 struct AlbumListReducer: ReducerProtocol {
     
     struct State: Equatable {
-        @BindingState fileprivate var albums: [AlbumEntity] = [
-            AlbumEntity(id: 1, userId: 1, title: "hi"),
-            AlbumEntity(id: 2, userId: 1, title: "hi"),
-            AlbumEntity(id: 3, userId: 1, title: "hi"),
-            AlbumEntity(id: 4, userId: 13, title: "hi"),
-        ]
+        @BindingState fileprivate var albums: [AlbumWithImageEntity] = []
         @BindingState var filteredUserId: String = ""
         var sectionByUsers: Bool = false
     
-        var albumsToShow: [AlbumEntity] {
+        var albumsToShow: [AlbumWithImageEntity] {
             guard filteredUserId.isEmpty == false, let userId = Int(filteredUserId) else { return albums }
             return albums
                 .filter { $0.userId == userId }
                 .sorted { $0.userId < $1.userId }
         }
         
-        var albumsGroupedByUserId: [[AlbumEntity]] {
+        var albumsGroupedByUserId: [[AlbumWithImageEntity]] {
             return Dictionary(grouping: albumsToShow, by: \.userId)
                 .sorted(by: { $0.key < $1.key })
                 .map { $0.value }
@@ -37,7 +32,7 @@ struct AlbumListReducer: ReducerProtocol {
     enum Action: BindableAction {
         case binding(BindingAction<State>)
         case onAppear
-        case albumLoaded([AlbumEntity])
+        case albumLoaded([AlbumWithImageEntity])
         case sectionByUsersChanged
     }
 
