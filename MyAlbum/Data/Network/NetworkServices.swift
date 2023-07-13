99204fed2af753e7d3960dde44008a19cbd01758
Kiarash Vosough <vosough.k@gmail.com>
2023-07-13 22:22:55 +0330

    created instance for dependency managment

--- MyAlbum/Data/Network/NetworkServices.swift
+++ MyAlbum/Data/Network/NetworkServices.swift
@@ -9,19 +9,20 @@ import Foundation
 
 final class NetworkServices {
 
+    static let shared = NetworkServices()
+    
     private static var cacheMemoryCapacity: Int { Int(1.5e+7) }
     private static var cacheDiskCapacity: Int { Int(1e+8) }
     
     let session: URLSession
     let cache: URLCache
     
-    init() {
-        let configuration = URLSessionConfiguration.default
-        self.session = URLSession(configuration: configuration)
+    init(session: URLSession = URLSession(configuration: .default)) {
+        self.session = session
         self.cache = URLCache(
             memoryCapacity: NetworkServices.cacheMemoryCapacity,
             diskCapacity: NetworkServices.cacheDiskCapacity
         )
-        configuration.urlCache = self.cache
+        session.configuration.urlCache = self.cache
     }
 }
