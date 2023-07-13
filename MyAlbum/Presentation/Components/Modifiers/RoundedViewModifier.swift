2bcba28ce003fe3c28936f14fda7462e7f96c19c
Kiarash Vosough <vosough.k@gmail.com>
2023-07-14 00:54:53 +0330

    added new modofier

---
+++ MyAlbum/Presentation/Components/Modifiers/RoundedViewModifier.swift
@@ -0,0 +1,28 @@
+//
+//  RoundedViewModifier.swift
+//  MyAlbum
+//
+//  Created by Kiarash Vosough on 7/14/23.
+//
+
+import SwiftUI
+
+extension View {
+
+    public func roundedView() -> some View {
+        modifier(RoundedViewModifier())
+    }
+}
+
+private struct RoundedViewModifier: ViewModifier {
+
+    func body(content: Content) -> some View {
+        content
+            .padding(.all, 16)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(.secondary.opacity(0.5))
+            }
+    }
+}
