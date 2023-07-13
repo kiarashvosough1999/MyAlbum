c38d3765b975750cda924b4a73a23eb1406c65c2
Kiarash Vosough <vosough.k@gmail.com>
2023-07-13 15:04:19 +0330

    first commit

---
+++ MyAlbum/MyAlbumApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAlbumApp.swift
+//  MyAlbum
+//
+//  Created by Kiarash Vosough on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MyAlbumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EmptyView()
+        }
+    }
+}
