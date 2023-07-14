8007812f818b4c08f938bc18b4d870e49f765246
Kiarash Vosough <vosough.k@gmail.com>
2023-07-14 12:15:41 +0330

    added AllAlbumListViewModel

--- MyAlbum/Presentation/Components/SubViews/AllAlbumListView.swift
+++ MyAlbum/Presentation/Components/SubViews/AllAlbumListView.swift
@@ -6,26 +6,43 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
+
+// MARK: - ViewModel
+
+struct AllAlbumListViewModel: Identifiable {
+    let id: Int
+    let item: AlbumItemViewModel
+}
+
+extension AllAlbumListViewModel: Hashable {}
+
+// MARK: - View
 
 struct AllAlbumListView: View {
 
-    private let albums: [AlbumWithImageEntity]
+    private let albums: [AllAlbumListViewModel]
     
-    init(albums: [AlbumWithImageEntity]) {
+    init(albums: [AllAlbumListViewModel]) {
         self.albums = albums
     }
     
     var body: some View {
-        ScrollView {
-            LazyVStack {
-                ForEach(albums) { album in
-                    AlbumItemView(model: album)
-                        .equatable()
-                }
+        ScrollableLazyVStack(dataSource: albums) { album in
+            let state = Path.State.showPhotos(
+                photos: PhotosListReducer.State.init(albumId: album.id)
+            )
+            NavigationLink(state: state) {
+                AlbumItemView(model: album.item)
+                    .equatable()
+                    .roundedView()
+                    .padding(.horizontal, 16)
             }
         }
     }
 }
+
+// MARK: - Equatable
 
 extension AllAlbumListView: Equatable {
     static func == (lhs: AllAlbumListView, rhs: AllAlbumListView) -> Bool {
