ff68303e87f0520121d261faf564dcc647be1370
Kiarash Vosough <vosough.k@gmail.com>
2023-07-14 12:19:46 +0330

    implemented uitests

---
+++ MyAlbumUITests/AlbumListUITests.swift
@@ -0,0 +1,52 @@
+//
+//  AlbumListUITests.swift
+//  MyAlbumUITests
+//
+//  Created by Kiarash Vosough on 7/13/23.
+//
+
+import XCTest
+
+final class AlbumListUITests: XCTestCase {
+
+    private var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        _ = app.waitForExistence(timeout: 5)
+    }
+
+    override func tearDownWithError() throws {
+        app = nil
+    }
+
+    func testNavigationBars() throws {
+        XCTAssertEqual(app.navigationBars.element.identifier, "Albums")
+    }
+    
+    func testButtons() throws {
+        XCTAssert(app.buttons.count > 4)
+    }
+    
+    func testScrollView() throws {
+        let element = app.scrollViews.element
+        XCTAssertEqual(element.exists, true)
+    }
+    
+    func testImages() throws {
+        let element = app.images.element
+        XCTAssert(app.images.count > 4)
+        XCTAssert(element.exists)
+    }
+    
+    func testTexts() throws {
+        let element = app.staticTexts
+        
+        let query = element.matching(NSPredicate(format: "label == %@", "Albums"))
+        
+        XCTAssert(query.element.exists)
+        XCTAssert(element.count > 4)
+    }
+}
